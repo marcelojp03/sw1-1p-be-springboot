d2477119e6d7a996b671d0ddc1d142f7c2f9cd61
Marcelo <marcelojp03@gmail.com>
2026-04-26 17:46:01 -0400

    fix(api): habilitar PageSerializationMode.VIA_DTO para serialización estable de Page

--- src/main/java/sw1/p1/P1Application.java
+++ src/main/java/sw1/p1/P1Application.java
@@ -2,8 +2,10 @@ package sw1.p1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class P1Application {
 
 	public static void main(String[] args) {
